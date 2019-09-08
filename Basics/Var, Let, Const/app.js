800f753b23ae56e417246bc8582217fa1f0bce83
cengamat <7.horcruxe@windowslive.com>
2019-09-08 17:42:01 +0300

    Var Let Const

---
+++ Basics/Var, Let, Const/app.js
@@ -0,0 +1,35 @@
+// Var
+
+// var name = "Ahmet Zurnaci";
+// var name = "Harry Potter";
+// console.log(name);
+
+// name = "Peter";
+// console.log(name);
+
+
+// Let
+
+// let name = "Peter Parker";
+// console.log(name);
+
+// let name = "Harry";
+// console.log(name);
+
+// Const
+
+// const name = "Peter Parker";
+
+// console.log(name);
+
+// name = "Harry";
+
+// console.log(name);
+
+// const a = [1, 2, 3, 4, 5];
+
+// console.log(a);
+
+// a.push(6);
+// console.log(a);
+
