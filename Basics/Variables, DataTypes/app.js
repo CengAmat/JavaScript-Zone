d8cb4612140b6d0b21adfa5257860b4328f5e73c
cengamat <7.horcruxe@windowslive.com>
2019-09-08 17:04:43 +0300

    First commit

---
+++ Basics/Variables, DataTypes/app.js
@@ -0,0 +1,78 @@
+//  Creating Variable
+
+// var a = 20;
+// var b = 10;
+// var c = 40;
+
+// console.log(a, b, c);
+
+// Primitive
+
+// var a = 10; // Number Data Type
+// var b = 3.14; // Number Data Type
+// console.log(typeof a);
+// console.log(typeof b);
+
+// String
+
+// var name = "Mustafa";
+// console.log(name);
+// console.log(typeof name);
+
+// Boolean
+
+// var a = true;
+// console.log(typeof a);
+
+// var a = null;
+// console.log(a);
+// console.log(typeof a);
+
+// var a;
+// console.log(a);
+// console.log(typeof a);
+
+//  Reference  Data Types
+
+// var numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(numbers[0]);
+
+// var person = {
+//     name: "Ahmet Zurnacı",
+//     age: 30
+// }
+// console.log(person);
+// console.log(typeof person);
+
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+// var merhaba = function () {
+//     console.log("Hello");
+// }
+
+// console.log(merhaba);
+// console.log(typeof merhaba);
+
+// var a = 10;
+
+// var b = a;
+
+// console.log(a, b);
+
+// var a = 20;
+
+// console.log(a, b);
+
+
+var a = [1, 2, 3];
+
+var b = a;
+a.push(4);
+
+console.log(b);
+
