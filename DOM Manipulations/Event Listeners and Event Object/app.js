9aeadb98023ae43014f77f8831c44ab986b42c44
cengamat <7.horcruxe@windowslive.com>
2019-09-21 17:29:24 +0300

    EventListener

---
+++ DOM Manipulations/Event Listeners and Event Object/app.js
@@ -0,0 +1,32 @@
+const filterInput = document.getElementById("filter");
+
+const todoForm = document.getElementById("todo-form");
+todoForm.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    console.log("Submit Event");
+
+    e.preventDefault();
+}
+
+
+
+// filterInput.addEventListener("focus", function (e) {
+
+//     console.log(e);
+//     console.log(e.type);
+//     console.log(e.target);
+//     console.log(e.target.placeholder);
+//     console.log(e.target.className);
+//     // console.log("Naber");
+// })
+
+// filterInput.onfocus = function () {
+//     console.log("Naber");
+// }
+
+
+
+
+
+// console.log(filterInput);
