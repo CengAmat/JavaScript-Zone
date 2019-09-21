a66ea11766ba408954a88d761e838c9edc093c3a
cengamat <7.horcruxe@windowslive.com>
2019-09-21 18:49:16 +0300

    Input Events

---
+++ DOM Manipulations/Input Events/app.js
@@ -0,0 +1,29 @@
+const filter = document.getElementById("filter");
+
+// document.addEventListener("DOMContentLoaded", load);
+
+// function load(e) {
+//     console.log("Page Loaded..");
+// }
+
+// Focus
+// filter.addEventListener("focus", run);
+
+// Blur
+// filter.addEventListener("blur", run);
+
+// Paste
+// filter.addEventListener("paste", run);
+
+// Copy
+// filter.addEventListener("copy", run);
+
+// Cut
+// filter.addEventListener("cut", run);
+
+// Select
+filter.addEventListener("select", run);
+
+function run(e) {
+    console.log(e.type);
+}
