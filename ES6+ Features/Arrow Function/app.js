8fcfe9c2aef247fb507c3b4b1cfec50920acf81d
cengamat <7.horcruxe@windowslive.com>
2019-09-23 22:27:11 +0300

    ES Features - Arrow Function

---
+++ ES6+ Features/Arrow Function/app.js
@@ -0,0 +1,33 @@
+// const hello = function () {
+//     console.log("Hello");
+// }
+
+// hello();
+
+// Arrow Function
+
+// const hello = () => {
+//     console.log("hello");
+// }
+
+// hello();
+
+// const hello = firstName => console.log("Merhaba, ", firstName);
+// const hello = (firstName, lastName) => console.log("Merhaba, ", firstName, lastName);
+
+// hello("Amat", "Zurnaci");
+
+// const cube = function (x) {
+//     return x * x * x;
+// }
+
+// console.log(cube(4));
+
+// const cube = x => {
+//     return x * x * x;
+// }
+
+const cube = x => x * x * x;
+
+
+console.log(cube(4));
