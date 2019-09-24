4b850c71bc695bad46d65847971f6e71c586531c
cengamat <7.horcruxe@windowslive.com>
2019-09-24 19:32:39 +0300

    Reference Types

---
+++ ES6+ Features/Reference Types/app.js
@@ -0,0 +1,33 @@
+// let a = "Amat";
+// let b = "Amat";
+
+// if (a == b) {
+//     console.log("Equal")
+// }
+
+// let array1 = [1, 2, 3, 4, 5];
+// let array2 = [1, 2, 3, 4, 5];
+
+// const person1 = {
+//     age: "Ahmet",
+//     age: 30
+// }
+
+// const person2 = {
+//     age: "Harry",
+//     age: 30
+// }
+
+// if (person1 === person2) {
+//     console.log("Equal");
+// }
+
+// const cities = new Map();
+// const key = [1, 2, 3];
+// cities.set("Ankara", 5);
+// cities.set("Istanbul", 15);
+// cities.set(key, "Array");
+
+
+// console.log(cities.get("Ankara"));
+// console.log(cities.get(key));
