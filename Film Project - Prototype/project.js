210a000fd3ddf3488c92c0b38319e9a7fdede378
cengamat <7.horcruxe@windowslive.com>
2019-09-30 21:49:16 +0300

    Film Project - UI and Notifications

---
+++ Film Project - Prototype/project.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// Initialize UI Object
+const ui = new UI();
+
+// Load all events
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit", addFilm);
+}
+
+function addFilm(e) {
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+    if (title == "" || director == "" || url == "") {
+        ui.displayMessages("Fill empty fields..", "danger");
+    }
+    else {
+        // New Film
+        const newFilm = new Film(title, director, url);
+        ui.addFilmToUI(newFilm); // Add Film to UI
+        ui.displayMessages("Film Added Succesfully", "success")
+    }
+    ui.clearInputs(titleElement, directorElement, urlElement);
+    e.preventDefault();
+}
