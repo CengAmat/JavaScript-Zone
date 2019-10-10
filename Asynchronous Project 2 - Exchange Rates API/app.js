3791c34e1719930a32e292adfa8d33e0a372f6cf
cengamat <7.horcruxe@windowslive.com>
2019-10-10 23:06:53 +0300

    Exchange API

---
+++ Asynchronous Project 2 - Exchange Rates API/app.js
@@ -0,0 +1,25 @@
+// Select Elements
+const amountElement = document.querySelector("#amount");
+const firstSelect = document.querySelector("#firstCurrency");
+const secondSelect = document.querySelector("#secondCurrency");
+const currency = new Currency("USD", "TRY");
+
+eventListener();
+
+function eventListener() {
+    amountElement.addEventListener("input", exchangeCurrency);
+    firstSelect.onchange = function () {
+        currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent);
+    };
+    secondSelect.onchange = function () {
+        currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
+
+    };
+}
+
+function exchangeCurrency() {
+    currency.changeAmount(amountElement.value);
+    currency.exchange()
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
+}
