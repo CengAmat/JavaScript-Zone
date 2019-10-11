9b3b611fa31b66fdffdf29d287ddd2cf0e89cc24
cengamat <7.horcruxe@windowslive.com>
2019-10-12 00:53:32 +0300

    User Interface

--- Asynchronous Project 2 - Exchange Rates API/app.js
+++ Asynchronous Project 2 - Exchange Rates API/app.js
@@ -3,6 +3,7 @@ const amountElement = document.querySelector("#amount");
 const firstSelect = document.querySelector("#firstCurrency");
 const secondSelect = document.querySelector("#secondCurrency");
 const currency = new Currency("USD", "TRY");
+const ui = new UI(firstSelect, secondSelect);
 
 eventListener();
 
@@ -10,16 +11,19 @@ function eventListener() {
     amountElement.addEventListener("input", exchangeCurrency);
     firstSelect.onchange = function () {
         currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent);
+        ui.changeFirst();
     };
     secondSelect.onchange = function () {
         currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent);
-
+        ui.changeSecond();
     };
 }
 
 function exchangeCurrency() {
     currency.changeAmount(amountElement.value);
     currency.exchange()
-        .then(result => console.log(result))
+        .then(result => {
+            ui.displayResult(result)
+        })
         .catch(err => console.log(err));
 }
