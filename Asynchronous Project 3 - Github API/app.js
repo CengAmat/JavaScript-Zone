52b8c3bcfa238c12495940d0cd4bab228cbdd027
cengamat <7.horcruxe@windowslive.com>
2019-10-12 17:34:46 +0300

    User Interface

--- Asynchronous Project 3 - Github API/app.js
+++ Asynchronous Project 3 - Github API/app.js
@@ -4,6 +4,7 @@ const nameInput = document.getElementById("githubname");
 const clearLastUsers = document.getElementById("clear-last-users");
 const lastUsers = document.getElementById("last-users");
 const github = new Github();
+const ui = new UI();
 
 eventListeners();
 
@@ -26,14 +27,14 @@ function getData(e) {
                     console.log("Error");
                 }
                 else {
-                    console.log(response);
+                    ui.showUserInfo(response.user);
                 }
             })
             .catch(err => console.log(err));
 
     }
 
-
+    ui.clerInput();
     e.preventDefault();
 }
 
